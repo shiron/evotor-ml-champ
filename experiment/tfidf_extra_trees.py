0aa2e5cc6f4d871f285ac7b0ec44a6d7f915ee5c
shiron <p.stashevsky@2gis.ru>
2017-06-06 17:56:11 +0700

    add xgb, nb, extra trees

--- experiment/tfidf_extra_trees.py
+++ experiment/tfidf_extra_trees.py
@@ -2,11 +2,9 @@
 
 from hyperopt import fmin, tpe, hp, Trials, STATUS_OK
 import pandas as pd
-import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import cross_val_score
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import ExtraTreesClassifier
 from xgboost import XGBClassifier
 
 train_data = pd.read_csv('./../data/lem_train_data.csv', sep=';', header=0)
@@ -22,7 +20,7 @@ def hyperopt_rf_opt(params):
     del params['features']
 
     # learn classifier
-    clf = XGBClassifier(**params)
+    clf = ExtraTreesClassifier(**params)
     scores = cross_val_score(clf, cv_train_data, train_data['GROUP_ID'], scoring='accuracy', cv=3).mean()
     print "Scores : "
     print scores.mean()
@@ -32,41 +30,40 @@ def f(params):
     acc = hyperopt_rf_opt(params)
     return {'loss': 1-acc, 'status': STATUS_OK}
 
-random_state = 27
+random_state = 11
 n_jobs = 4
-max_evals = 10
+criterion = ['gini', 'entropy']
+max_evals = 20
 
-space4xgb = {
-    'features': hp.choice('features', range(4000, 15000)),
-    'n_estimators': hp.choice('n_estimators', range(50,450)),
-    'max_depth': hp.choice('max_depth', range(1,5)),
-    'learning_rate': hp.uniform('learning_rate', 0, 1),
-    'objective': 'multi:softmax',
-    'seed': random_state,
-    'nthread': n_jobs
+space4et = {
+    'features': hp.choice('features', range(4000, 20000)),
+    'n_estimators': hp.choice('n_estimators', range(100,600)),
+    'criterion': hp.choice('criterion', ["gini", "entropy"]),
+    'random_state': random_state,
+    'n_jobs': n_jobs
 
 }
 
 
-#trials = Trials()
-#best = fmin(f, space4xgb, algo=tpe.suggest, max_evals=max_evals, trials=trials)
-#print 'best:'
-#print best
-best = {'n_estimators': 300, 'learning_rate': 0.39712616964814607, 'features': 12000, 'max_depth': 4}
+trials = Trials()
+best = fmin(f, space4et, algo=tpe.suggest, max_evals=max_evals, trials=trials)
+print 'best:'
+print best
 
 cv = TfidfVectorizer(max_features=best['features'])
 cv_train_data = cv.fit_transform(train_data['NAME'])
 cv_test_data = cv.transform(test_data['NAME'])
 
-best = XGBClassifier(learning_rate=best['learning_rate'], n_estimators=best['n_estimators'], max_depth=best['max_depth'],
-                          seed=random_state, objective='multi:softmax', nthread=n_jobs)
+best = ExtraTreesClassifier(n_estimators=best['n_estimators'], criterion=criterion[best['criterion']],
+                            random_state=random_state, n_jobs=n_jobs)
 
 print 'Score of best: '
 print cross_val_score(best, cv_train_data, train_data['GROUP_ID'], cv=3, scoring='accuracy').mean()
 
+clf = ExtraTreesClassifier(n_estimators=best['n_estimators'], criterion=criterion[best['criterion']],
+                            random_state=random_state, n_jobs=n_jobs)
+clf.fit(cv_train_data, train_data['GROUP_ID'])
 
-best.fit(cv_train_data, train_data['GROUP_ID'])
-
-predict_clf = best.predict(cv_test_data)
+predict_clf = clf.predict(cv_test_data)
 test_data['GROUP_ID'] = predict_clf
-test_data[['id', 'GROUP_ID']].to_csv('./../result/res_xgb_0606.csv', sep=',', header=True, index=False)
+test_data[['id', 'GROUP_ID']].to_csv('./../result/res_et_0606.csv', sep=',', header=True, index=False)
--- experiment/tfidf_nb.py
+++ experiment/tfidf_nb.py
@@ -2,12 +2,10 @@
 
 from hyperopt import fmin, tpe, hp, Trials, STATUS_OK
 import pandas as pd
-import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import cross_val_score
-from sklearn.ensemble import RandomForestClassifier
-from xgboost import XGBClassifier
+from sklearn.naive_bayes import MultinomialNB
 
 train_data = pd.read_csv('./../data/lem_train_data.csv', sep=';', header=0)
 test_data = pd.read_csv('./../data/lem_test_data.csv', sep=';', header=0)
@@ -22,8 +20,8 @@ def hyperopt_rf_opt(params):
     del params['features']
 
     # learn classifier
-    clf = XGBClassifier(**params)
-    scores = cross_val_score(clf, cv_train_data, train_data['GROUP_ID'], scoring='accuracy', cv=3).mean()
+    clf = MultinomialNB(**params)
+    scores = cross_val_score(clf, cv_train_data, train_data['GROUP_ID'], cv=3, scoring='accuracy')
     print "Scores : "
     print scores.mean()
     return scores.mean()
@@ -32,34 +30,27 @@ def f(params):
     acc = hyperopt_rf_opt(params)
     return {'loss': 1-acc, 'status': STATUS_OK}
 
-random_state = 27
-n_jobs = 4
-max_evals = 10
+random_state = 1
+n_jobs = 8
+max_evals = 50
 
-space4xgb = {
-    'features': hp.choice('features', range(4000, 15000)),
-    'n_estimators': hp.choice('n_estimators', range(50,450)),
-    'max_depth': hp.choice('max_depth', range(1,5)),
-    'learning_rate': hp.uniform('learning_rate', 0, 1),
-    'objective': 'multi:softmax',
-    'seed': random_state,
-    'nthread': n_jobs
-
+space4nb = {
+    'features': hp.choice('features', range(2000, 20000)),
+    'alpha': hp.uniform('alpha', 0, 1)
 }
 
 
-#trials = Trials()
-#best = fmin(f, space4xgb, algo=tpe.suggest, max_evals=max_evals, trials=trials)
-#print 'best:'
-#print best
-best = {'n_estimators': 300, 'learning_rate': 0.39712616964814607, 'features': 12000, 'max_depth': 4}
+trials = Trials()
+best = fmin(f, space4nb, algo=tpe.suggest, max_evals=max_evals, trials=trials)
+print 'best:'
+print best
+
 
 cv = TfidfVectorizer(max_features=best['features'])
 cv_train_data = cv.fit_transform(train_data['NAME'])
 cv_test_data = cv.transform(test_data['NAME'])
 
-best = XGBClassifier(learning_rate=best['learning_rate'], n_estimators=best['n_estimators'], max_depth=best['max_depth'],
-                          seed=random_state, objective='multi:softmax', nthread=n_jobs)
+best = MultinomialNB(alpha=best['alpha'])
 
 print 'Score of best: '
 print cross_val_score(best, cv_train_data, train_data['GROUP_ID'], cv=3, scoring='accuracy').mean()
@@ -69,4 +60,4 @@ best.fit(cv_train_data, train_data['GROUP_ID'])
 
 predict_clf = best.predict(cv_test_data)
 test_data['GROUP_ID'] = predict_clf
-test_data[['id', 'GROUP_ID']].to_csv('./../result/res_xgb_0606.csv', sep=',', header=True, index=False)
+test_data[['id', 'GROUP_ID']].to_csv('./../result/res_nb_0606.csv', sep=',', header=True, index=False)
